78e92af8b92b1624ea42b2887ba041bb67d9d118
z720 <Seb@z720.net>
2013-07-24 11:46:00 -0400

    adding public folder

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>z720.net</title>
+</head>
+<body class="home">
+    <header>
+        <h1>z720.net</h1>
+    </header>
+    <section class="main">
+        Nothing yet... (It's still a beta) 
+    </section>
+    <footer>
+        <p>&copy; 1997-<?php echo date("Y") ?> Sébastien Erard</p>
+    </footer>
+</body>
+</html>
