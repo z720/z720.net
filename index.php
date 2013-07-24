eee0d585b014df2e0760f5a155ac9b6261bd1e31
z720 <Seb@z720.net>
2013-07-24 05:02:39 -0400

    Friendly index (placeholder)

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
-<?php 
-require('./wp/wp-blog-header.php');
+<!DOCTYPE html>
+<html>
+<head>
+    <title>z720.net</title>
+</head>
+<body class="home">
+    <header>
+        <h1>z720.net</h1>
+    </header>
+    <section class="main">
+        Nothing yet... (It's still a beta) 
+    </section>
+    <footer>
+        <p>&copy; 1997-<?php echo date("Y") ?> Sébastien Erard</p>
+    </footer>
+</body>
+</html>
