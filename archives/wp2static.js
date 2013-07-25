c12d5021fa8b5cf7be429320c836ac9f1f499208
z720 <Seb@z720.net>
2013-07-25 03:54:45 -0400

    Extracting wordpress archives é

---
+++ archives/wp2static.js
@@ -0,0 +1,75 @@
+var fs = require('fs'),
+    path = require('path'),
+    xml2js = require('xml2js'),
+    events = require('events'),
+    u = require('url'),
+    mkdirp = require('mkdirp'),
+    http = require('http');
+
+var file = process.argv[2];
+
+var wp = new events.EventEmitter();
+
+var parser = new xml2js.Parser();
+parser.addListener('end', function(result) {
+	parseXml(result);
+});
+
+var parseXml = function(xml) {
+	parseChannel(xml.rss.channel[0]);
+};
+
+var parseChannel = function(channel) {
+	for(var index in channel.item) {
+		parseItem(channel.link, channel.item[index])
+	}
+}
+
+var parseItem = function(root, item) {
+	var folder = item.link[0].replace(root,'');
+    if(['publish','inherit'].indexOf(item['wp:status'][0]) > -1 ) {
+        if(folder.indexOf('?') == -1) {
+            var dest = './archives/wp'+folder,
+                filename = '_'+item['wp:post_type'][0]+'.v05.json';
+            mkdirp(dest, function(err) {
+                if (err) {
+                    console.error(err);
+                } else {
+                    var article = JSON.stringify(item);
+                    fs.writeFile(dest+'/' + filename, article, function(err) {
+                        if(err) {
+                            console.error('filename: '+err);
+                        } else {
+                            console.info('> '+dest)
+                        }
+                    });
+                }
+            });
+        } else {
+            if(item['wp:post_type'][0] == 'attachment') {
+                // download
+                // put in the righ place
+                console.info('>> '+item['wp:attachment_url'][0]);
+                var dest = item['wp:attachment_url'][0].replace(root,'');
+                mkdirp(path.dirname('./archives/wp'+dest), function(err) {
+                    if(err) {
+                        console.error('dl: '+err);
+                    } else {
+                        var file = fs.createWriteStream('./archives/wp'+dest);
+                        var request = http.get(item['wp:attachment_url'][0], function(response) {
+                            response.pipe(file);
+                        });
+                    }
+                });
+            } else {
+                console.warn('WTF: '+item.title[0] + ' -> '+item.link[0]);    
+            }
+        }
+    } else {
+	    //console.info('Ignored:' + item['wp:status'][0]+ " - " + folder);
+    }
+}
+
+fs.readFile(file, function(err, data) {
+    parser.parseString(data);
+});
