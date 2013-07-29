31a722dc5e0513fa3aec3de0e803d624271f938f
z720 <Seb@z720.net>
2013-07-29 05:11:35 -0400

    Move to Grunt'

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    wintersmith: {
+      config: "./config.json"
+    }
+  });
+
+  grunt.registerTask('wintersmith', 'Wintersmith build task', function() {
+    grunt.config.requires('wintersmith.config');
+    var done = this.async();
+    var wintersmith = require('wintersmith');
+    var gen = wintersmith(grunt.config('wintersmith.config'));
+    gen.build(function(err) {
+      if(err) { 
+        grunt.log.error("Wintersmith Step: error: "+err);
+        done(false);
+      }
+      grunt.log.writeln('Wintersmith Step: done!');
+      done();
+    });
+  });
+
+  // Default task(s).
+  grunt.registerTask('build', ['wintersmith']);
+  grunt.registerTask('default', ['build']);
+
+};
--- build.js
+++
@@ -1,8 +0,0 @@
-var wintersmith = require('wintersmith');
-
-
-var gen = wintersmith('./config.json');
-gen.build(function(err) {
-	if(err) throw err;
-	console.log("Wintersmith Step: done!");
-});
