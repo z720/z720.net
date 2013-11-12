bd335c13ad5bf580700d9f5bfab106192bb8b2f4
z720 <Seb@z720.net>
2013-11-12 08:48:55 -0500

    Fix __DIR__ on Dreamhost

--- public/pico.php
+++ public/pico.php
@@ -1,6 +1,12 @@
 <?php
 
+// TODO: include in Pico
 date_default_timezone_set('Europe/Paris');
+
+// TODO: fill issue
+if(!is_defined(__DIR__)) {
+	define('__DIR__',  dirname(__FILE__));
+}
 
 define('ROOT_DIR', realpath(dirname(__FILE__)) .'/');
 define('CONTENT_DIR', ROOT_DIR .'content/');
