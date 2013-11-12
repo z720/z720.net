727b3f8ca0f016f609ebb6eebe167e5d8780e4f3
z720 <Seb@z720.net>
2013-11-12 03:28:04 -0500

    Installation Pico

--- public/index.php
+++ public/index.php
@@ -1,36 +1,13 @@
-<!DOCTYPE html>
-<html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width">
-    <title>Welcome - z720.net
-    </title>
-    <link rel="alternate" href="http://z720.net/feed.xml" type="application/rss+xml" title="">
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic|Anonymous+Pro:400,700,400italic,700italic|Merriweather:400,700,300">
-  </head>
-  <body>
-    <header class="header">
-      <div class="content-wrap">
-        <h1>Welcome</h1>
-      </div>
-    </header>
-    <div id="content">
-      <div class="content-wrap">
-        <article class="article">
-          <section class="content"><h2>Hello</h2>
-<p>Bienvenue sur <a href="http://z720.net/">z720.net</a>.</p>
-<p>Nothing yet… (It&#39;s still a beta) </p>
-<p><a href="https://drone.io/github.com/z720/z720.net/latest"><img src="https://drone.io/github.com/z720/z720.net/status.png" alt="Build Status"></a></p>
-</section>
-        </article>
-      </div>
-    </div>
-    <footer>
-      <section class="copy">
-        <p>&copy; 1997-2013 z720 &mdash; powered by&nbsp;<a href="https://github.com/jnordberg/wintersmith">Wintersmith</a>
-        </p>
-      </section>
-    </footer>
-  </body>
-</html>
+<?php
+
+define('ROOT_DIR', realpath(dirname(__FILE__)) .'/');
+define('CONTENT_DIR', ROOT_DIR .'content/');
+define('CONTENT_EXT', '.md');
+define('LIB_DIR', ROOT_DIR .'lib/');
+define('PLUGINS_DIR', ROOT_DIR .'plugins/');
+define('THEMES_DIR', ROOT_DIR .'themes/');
+define('CACHE_DIR', LIB_DIR .'cache/');
+
+require(ROOT_DIR .'vendor/autoload.php');
+require(LIB_DIR .'pico.php');
+$pico = new Pico();
