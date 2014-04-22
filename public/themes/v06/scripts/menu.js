4b9afedf68f945f42ef4bf80365ad2bed2c13aab
z720 <seb@z720.net>
2014-04-22 13:53:03 +0200

    Design: Menu mobile

---
+++ public/themes/v06/scripts/menu.js
@@ -0,0 +1,7 @@
+if (window.innerWidth<770) {
+    t = document.getElementById('navigation');
+    if(t) {
+        t.classList.toggle('nav-opened');
+        t.onclick=function(){ alert;t.classList.toggle('nav-opened');}
+    }
+}
