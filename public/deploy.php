bd15cecd6a1639f2e3abebdc0b44b3a91af590bd
z720 <Seb@z720.net>
2014-07-25 09:34:46 +0000

    Update script

--- public/deploy.php
+++ public/deploy.php
@@ -34,7 +34,7 @@
 <html lang="en-US">
 <head>
 	<meta charset="UTF-8">
-	<title>GIT DEPLOYMENT SCRIPT</title>
+	<title>DEPLOYMENT SCRIPT</title>
 </head>
 <body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
 <pre>
