52a1b84c42aa7e0bb7c492da146cc722deb24c51
z720 <seb@z720.net>
2014-08-04 18:43:07 +0200

    Fix date format

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 $config['site_title'] = 'z720.net';			// Site title
 //$config['base_url'] = ''; 				// Override base URL (e.g. http://example.com)
 $config['theme'] = 'v06'; 			// Set the theme (defaults to "default")
-$config['date_format'] = 'j/m/Y';		// Set the PHP date format
+$config['date_format'] = 'd/m/Y';		// Set the PHP date format
 $config['twig_config'] = array(			// Twig settings
 	'cache' => CACHE_DIR,					// To enable Twig caching change this to CACHE_DIR
 	'autoescape' => false,				// Autoescape Twig vars
