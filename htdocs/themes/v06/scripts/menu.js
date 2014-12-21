79fa9322c0a544ef011c0a035880916b16618a54
z720 <seb@z720.net>
2014-12-21 14:54:27 +0100

    Menu improvment on smallscreen

--- htdocs/themes/v06/scripts/menu.js
+++ htdocs/themes/v06/scripts/menu.js
@@ -1,3 +1,4 @@
+/*
 if (window.innerWidth<770) {
     t = document.getElementById('navigation');
     if(t) {
@@ -5,3 +6,4 @@ if (window.innerWidth<770) {
         t.onclick=function(){ t.classList.toggle('nav-opened');}
     }
 }
+*/
