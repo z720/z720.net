e886ccdb6b447d149153f43b4725b9722015ccb2
z720 <seb@z720.net>
2014-12-21 14:36:55 +0100

    Fix content directory

--- htdocs/index.php
+++ htdocs/index.php
@@ -3,12 +3,12 @@ error_reporting(0); // no errors in production
 date_default_timezone_set('Europe/Paris');
 
 define('ROOT_DIR', realpath(dirname(__FILE__).'/../') .'/');
-define('CONTENT_DIR', ROOT_DIR . 'public/' );
+define('CONTENT_DIR', ROOT_DIR . basename(dirname(__FILE__)) . '/' );
 define('CONTENT_EXT', '.md');
 define('LIB_DIR', ROOT_DIR .'vendor/picocms/pico/lib/');
-define('PLUGINS_DIR', ROOT_DIR .'public/plugins/');
-define('THEMES_DIR', ROOT_DIR .'public/themes/');
-define('CACHE_DIR', ROOT_DIR .'cache/');
+define('PLUGINS_DIR', CONTENT_DIR . 'plugins/');
+define('THEMES_DIR', CONTENT_DIR . 'themes/');
+define('CACHE_DIR', ROOT_DIR . 'cache/');
 
 require(ROOT_DIR .'vendor/autoload.php');
 require(LIB_DIR .'pico.php');
