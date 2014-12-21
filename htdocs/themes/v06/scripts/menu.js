e263afaf26fa35a5145cc9055caaf26d458e7f44
z720 <seb@z720.net>
2014-12-21 14:15:32 +0100

    FIx alert in script

--- htdocs/themes/v06/scripts/menu.js
+++ htdocs/themes/v06/scripts/menu.js
@@ -2,6 +2,6 @@ if (window.innerWidth<770) {
     t = document.getElementById('navigation');
     if(t) {
         t.classList.toggle('nav-opened');
-        t.onclick=function(){ alert;t.classList.toggle('nav-opened');}
+        t.onclick=function(){ t.classList.toggle('nav-opened');}
     }
 }
