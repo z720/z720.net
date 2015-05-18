b27a6ed58bf6da7fb98418140f94c1149e12aa14
z720 <seb@z720.net>
2015-05-18 15:08:24 +0200

    Deploy script on location (+missing dev dependencies)

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-echo $PWD
+ORI=`pwd`
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+cd $DIR
+pwd
 whoami
 echo "------- Update site"
 git pull
 git status
 echo "------- Flag version/revision"
 git log --pretty=format:\'%h\' -n 1 > build
-echo "------- Update runnning dependencies"
-composer install
+echo "------- Update running dependencies"
+php composer install
 echo "------- Clear template cache:"
-rm -rvf ../cache/twig/??
 rm -rvf ./cache/twig/??
+
+cd $ORI
+pwd
