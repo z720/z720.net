b581f765272d3428c4bf320049836bad3acb315b
z720 <seb@z720.net>
2015-05-18 11:56:38 +0200

    Update deploy script: composer is mandatory at project root or in PATH

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,13 @@
 
 echo $PWD
 whoami
+echo "------- Update site"
 git pull
 git status
+echo "------- Flag version/revision"
 git log --pretty=format:\'%h\' -n 1 > build
-echo "Clear template cache:"
+echo "------- Update runnning dependencies"
+composer install
+echo "------- Clear template cache:"
 rm -rvf ../cache/twig/??
 rm -rvf ./cache/twig/??
